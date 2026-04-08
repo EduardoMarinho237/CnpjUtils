a6a439756843bbfa66a5d2a8926248c5e481e039
Eduardo Marinho <edu.docxl@gmail.com>
2026-04-08 07:28:39 -0300

    Adjsusts Swagger UI description text

--- src/main/java/com/eduardomarinho/cnpjutils/config/OpenApiConfig.java
+++ src/main/java/com/eduardomarinho/cnpjutils/config/OpenApiConfig.java
@@ -15,7 +15,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("CNPJ Utils API")
                         .version("1.0.0")
-                        .description("API para validação e geração de CNPJs nos padrões novo e antigo")
+                        .description("API para validação e geração de CNPJs nos padrões novo e antigo, a partir de julho de 2026, o formato do CNPJ sofrerá uma mudança, que não afeta os CNPJs já existentes mas altera o padrão dos novos CNPJs. Essa API foi criada para facilitar a geração e validação tanto desses novos CNPJs quanto dos antigos, o código está disponível no GitHub: https://github.com/EduardoMarinho237/CnpjUtils para que qualquer um possa ler e contribuir com melhorias.")
                         .license(new License()
                                 .name("MIT")
                                 .url("https://opensource.org/licenses/MIT")));
